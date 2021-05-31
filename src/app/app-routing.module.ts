a68f9fb76b0c9acb2ed66fd9937758b58cfb5707
chanshetty <ajinkya.chanshetty@ilmtecsolutions.com>
2021-05-31 21:09:09 +0530

    Updated the default route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,8 @@ import { ProjectsComponent } from './projects/projects.component';
 const routes: Routes = [
   {path:'dashboard', component:DashboardComponent},
   {path:'personal', component:PersonalComponent},
-  {path:'projects', component:ProjectsComponent}
+  {path:'projects', component:ProjectsComponent},
+  {path:'**', component:DashboardComponent},
 ];
 
 
